525494598ac7da087995add2062b935d50726733
Furqan Saeed <39068768+furqan1984@users.noreply.github.com>
2020-11-24 13:11:11 -0500

    Added Context for the QA model

---
+++ context_map.py
@@ -0,0 +1,68 @@
+context = '''
+TAM is Traceable Adjustable Market.
+RAM is Remaining Adjustable Market.
+My name is GLO-Operational Excellence Bot named by Patrick Hutchinson.
+TAM for LLL is 10,362.
+TAM for LJ is 10,711.
+TAM for NLJ is 1,488.
+TAM for Coaching is 10,711.
+TAM for Executive+ Coaching is 2,769.
+TAM for PoT is 9,257.
+TAM for VHC is 10,601.
+LLL RAM is 6,003.
+LJ Attainment is 90.
+LJ RAM is 10,621.
+LJ Attainment % is 0.84%.
+LLL Attainment % is 42.07%.
+NLJ Attainment is 312.
+NLJ RAM is 1,176.
+NLJ Attainment % is 20.97%.
+LLL Attainment is 4,359.
+Coaching Attainment is 320.
+Executive+ Coaching Attainment is 13.
+PoT Attainment is 4,155.
+VHC Attainment is 4,795.
+Coaching RAM is 10,391.
+Executive+ Coaching RAM is 2,756.
+PoT RAM is 5,102.
+VHC RAM is 5,806.
+Coaching Attainment % is 2.99%.
+Executive+ Coaching Attainment % is 0.47%.
+PoT Attainment % is 44.88%.
+VHC Attainment % is 45.23%.
+JUMP Attainment is 17.
+DARE Attainment is 1,719.
+NGL Attainment is 306.
+EGL Attainment is 34.
+Participation for Subscription is 15,110.
+Participation for Technical is 11,669.
+Participation for Security is 5,415.
+Participation for Software Development is 2,024.
+Participation for Cloud and Distributed Computing is 1,010.
+Participation for Networking Technologies is 959.
+Participation for Professional Development is 920.
+Participation for Data Science is 787.
+Participation for Cisco OS is 257.
+Participation for Product Management is 166.
+Participation for Hardware is 124.
+Participation for IoT is 7.
+Participation for Individual is 2,328.
+Participation for Power Skills is 1,351.
+Participation for Onboarding is 977.
+Participation for Every Leader & Team is 2,239.
+Participation for PoT is 1,243.
+Participation for LLL is 434.
+Participation for NLJ is 226.
+Participation for LJ is 161.
+Participation for Coaching is 161.
+Participation for Coaching Standalone is 13.
+Participation for Leader Day is 1.
+Participation for Inclusion & Collaboration is 678.
+Participation for JUMP is 362.
+Participation for DARE is 163.
+Participation for NGL is 116.
+Participation for EGL is 37.
+Participation for Critical Leaders & Teams is 172.
+Participation for Team Coaching is 172.
+
+'''
